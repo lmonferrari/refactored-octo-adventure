ccaec888cf21917da29f9b744a427cd1b8835eef
Luiz Monferrari <lmonferrrai@outlook.com>
2024-04-19 21:25:12 -0300

    predição

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
-from flask import Flask, request, render_template
+from joblib import load
+from flask import Flask, request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 DBPATH = os.path.join(BASEDIR, "db.sqlite3")
+ARTIFACTSPATH = os.path.join(BASEDIR, "artifacts/")
+
+model = load(ARTIFACTSPATH + "model.pkl")
+le_name = load(ARTIFACTSPATH + "le_name.pkl")
+le_packing = load(ARTIFACTSPATH + "le_packing.pkl")
 
 app = Flask(__name__)
 
@@ -29,6 +35,19 @@ def home():
     packing_types = {packing.packing for packing in packing_types}
 
     return render_template("index.html", packing_types=packing_types)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    packing = le_packing.transform([request.form.get("packing")])[0]
+    weight = int(request.form.get("weight"))
+
+    prediction = model.predict([[weight, packing]])[0]
+    prediction = le_name.inverse_transform([prediction])[0]
+
+    print(f"Prediction: {prediction}")
+
+    return jsonify({"product": prediction})
 
 
 if __name__ == "__main__":
